56144bddd5230860076f4f40ea714f11d2d93723
mdt053123 <mdt053123@gmail.com>
2025-11-08 12:23:48 -0500

    knn instructions

--- 1108-day7/knn.py
+++ 1108-day7/knn.py
@@ -20,3 +20,42 @@ def classify(p, k):
     """
     Can assume: k % 2 == 1 (odd), k <= len(data)
     """
+    
+    # first check if pt. exists
+    for point, color in points:
+        if point == p:
+            return color
+        
+    # next, collect distances w/ index for tiebreaking + colors
+    dists = []
+    idx = 0
+    for point, color in points:
+        d = distance(point, p)
+        dists.append((d, idx, color))
+        idx += 1
+        
+    # sort distances
+    dists_sorted = sorted(dists)
+    
+    # get k nearest neighbors
+    k_neighbors = dists_sorted[:k]
+    
+    # find the number of appearances of each color
+    counts = {}
+    for _, _, color in k_neighbors:
+        if color in counts:
+            counts[color] += 1
+        else:
+            counts[color] = 1
+    
+    # find color with max count
+    majority_color = None
+    majority_count = -1
+    for color, count in counts.items():
+        if count > majority_count:
+            majority_count = count
+            majority_color = color
+            
+    # store + return
+    points.append((p, majority_color))
+    return majority_color
